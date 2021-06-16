0dee3027c5413cfa3d63362a3d1f3299faba786b
Frederik Rommel <frederik.rommel@webidea24.de>
2021-06-16 15:17:03 +0200

    BILLSWVI-48: fix address correction on equal addresses

--- src/Components/Checkout/Controller/CheckoutController.php
+++ src/Components/Checkout/Controller/CheckoutController.php
@@ -14,15 +14,21 @@ namespace Billie\BilliePayment\Components\Checkout\Controller;
 use Billie\Sdk\Model\Address;
 use Billie\Sdk\Model\DebtorCompany;
 use Billie\Sdk\Model\Person;
+use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Checkout\Customer\SalesChannel\AccountService;
+use Shopware\Core\Checkout\Order\Aggregate\OrderAddress\OrderAddressEntity;
 use Shopware\Core\Checkout\Order\OrderEntity;
-use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SalesChannel\NoContentResponse;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -46,31 +52,60 @@ class CheckoutController extends StorefrontController
      */
     private $orderRepository;
 
+    /**
+     * @var \Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface
+     */
+    private $orderDeliveryRepository;
+
+    /**
+     * @var \Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @var \Shopware\Core\Checkout\Customer\SalesChannel\AccountService
+     */
+    private $accountService;
+
     public function __construct(
         EntityRepositoryInterface $addressRepository,
         EntityRepositoryInterface $orderRepository,
-        EntityRepositoryInterface $orderAddressRepository
+        EntityRepositoryInterface $orderAddressRepository,
+        EntityRepositoryInterface $orderDeliveryRepository,
+        AccountService $accountService
     ) {
         $this->customerAddressRepository = $addressRepository;
         $this->orderAddressRepository = $orderAddressRepository;
         $this->orderRepository = $orderRepository;
+        $this->orderDeliveryRepository = $orderDeliveryRepository;
+        $this->accountService = $accountService;
     }
 
     /**
      * @Route(path="/update-addresses/{orderId}", name="billie-payment.checkout.update-addresses", methods={"POST"}, defaults={"XmlHttpRequest"=true})
      * @noinspection NullPointerExceptionInspection
      *
-     * @return \Shopware\Core\System\SalesChannel\NoContentResponse|\Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @return NoContentResponse|NotFoundHttpException
      */
     public function updateCustomerAddress(Request $request, SalesChannelContext $salesChannelContext, string $orderId = null)
     {
+        //###############################################################################################################
+        //## PLEASE NOTE ################################################################################################
+        //## This action will update the address according the widget response.
+        //## Currently the billie payment methods are not available if the payment has been failed,
+        //## cause the customer can not change his address after the order has been placed. This is a concept of Shopware.
+        //## but this action has been already implemented the function to replace the addresses on a *placed* order.
+        //## currently the implementation (for the placed order) is not used, but has been fully tested.
+        //###############################################################################################################
+
         if ($orderId === null) {
             $this->updateAddress(
                 $salesChannelContext->getCustomer()->getActiveBillingAddress()->getId(),
                 $salesChannelContext->getCustomer()->getActiveShippingAddress()->getId(),
                 $request->request->all(),
                 $this->customerAddressRepository,
-                $salesChannelContext->getContext()
+                $salesChannelContext->getCustomer(),
+                $salesChannelContext
             );
         } else {
             $criteria = (new Criteria([$orderId]))
@@ -87,7 +122,8 @@ class CheckoutController extends StorefrontController
                 $orderEntity->getDeliveries()->first()->getShippingOrderAddressId(),
                 $request->request->all(),
                 $this->orderAddressRepository,
-                $salesChannelContext->getContext()
+                $orderEntity,
+                $salesChannelContext
             );
         }
 
@@ -99,8 +135,11 @@ class CheckoutController extends StorefrontController
         string $shopwareShippingAddressId,
         array $requestParams,
         EntityRepositoryInterface $repository,
-        Context $context
+        Entity $referencedEntity,
+        SalesChannelContext $salesChannelContext
     ): void {
+        $context = $salesChannelContext->getContext();
+
         $billieDebtorCompany = new DebtorCompany($requestParams['debtor_company']);
         $billieDebtorPerson = new Person($requestParams['debtor_person']);
         $billieShippingAddress = new Address($requestParams['delivery_address']);
@@ -116,14 +155,58 @@ class CheckoutController extends StorefrontController
             'city' => $billieDebtorCompany->getAddress()->getCity(),
         ]], $context);
 
-        if ($shopwareBillingAddressId !== $shopwareShippingAddressId) {
-            // update shipping address if the ids are not identical
-            $repository->update([[
-                'id' => $shopwareShippingAddressId,
+        if (!$this->compareArrays($billieDebtorCompany->getAddress()->toArray(), $billieShippingAddress->toArray())) {
+            $isNewAddress = $shopwareBillingAddressId === $shopwareShippingAddressId;
+            $shippingAddressData = [];
+            if ($isNewAddress) {
+                /** @var CustomerAddressEntity|OrderAddressEntity $billingAddressEntity */
+                $billingAddressEntity = $repository->search(new Criteria([$shopwareBillingAddressId]), $context)->first();
+
+                // copy current address data and remove specific values
+                $shippingAddressData = array_diff_key($billingAddressEntity->jsonSerialize(), array_flip([
+                    '_uniqueIdentifier', 'createdAt', 'updatedAt', 'order', 'customer', 'extensions', 'versionId', 'id', 'country', 'countryState', 'salutation', 'orderDeliveries',
+                ]));
+                $shippingAddressData['id'] = Uuid::randomHex();
+            } else {
+                $shippingAddressData['id'] = $shopwareShippingAddressId;
+            }
+
+            $shippingAddressData = array_merge($shippingAddressData, [
                 'street' => $billieShippingAddress->getStreet() . ' ' . $billieShippingAddress->getHouseNumber(),
                 'zipcode' => $billieShippingAddress->getPostalCode(),
                 'city' => $billieShippingAddress->getCity(),
-            ]], $context);
+            ]);
+            $repository->upsert([$shippingAddressData], $context);
+
+            if ($isNewAddress) {
+                if ($referencedEntity instanceof CustomerEntity) {
+                    $this->accountService->setDefaultShippingAddress($shippingAddressData['id'], $salesChannelContext);
+                } elseif ($referencedEntity instanceof OrderEntity) {
+                    /** @var \Shopware\Core\Checkout\Order\Aggregate\OrderDelivery\OrderDeliveryEntity $delivery */
+                    $delivery = $referencedEntity->getDeliveries()->first();
+                    $this->orderDeliveryRepository->upsert([[
+                        'id' => $delivery->getId(),
+                        'shippingOrderAddressId' => $shippingAddressData['id'],
+                    ]], $context);
+                }
+            }
         }
+    }
+
+    private function compareArrays(array $array1, array $array2): bool
+    {
+        if (count($array1) !== count($array2)) {
+            return false;
+        }
+
+        $keys = array_unique(array_merge(array_keys($array1), array_keys($array2)));
+
+        foreach ($keys as $key) {
+            if (($array1[$key] ?? null) !== ($array2[$key] ?? null)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
